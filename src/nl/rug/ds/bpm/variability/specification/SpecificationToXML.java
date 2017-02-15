4c48db2630c5d357522e2a8866818438c91a2571
nrtpvanbeest <nrtpvanbeest@users.noreply.github.com>
2017-02-15 12:35:06 +1000

    static class to generate variability spec xml

---
+++ src/nl/rug/ds/bpm/variability/specification/SpecificationToXML.java
@@ -0,0 +1,93 @@
+package nl.rug.ds.bpm.variability.specification;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SpecificationToXML {
+	
+	public static String getXML(VariabilitySpecification vs, String silentprefix) {
+		String xml = "";
+		List<String> labels = vs.getAllLabels();
+		Map<String, String> labelidmap = new HashMap<String, String>();
+		
+		int i = 0;
+		for (String lbl: labels) {
+			if ((!lbl.startsWith(silentprefix)) && (!lbl.equals("_0_")) && (!lbl.equals("_1_"))) {
+				labelidmap.put(lbl, "ap" + i);
+				i++;
+			}
+		}
+		
+		xml += "<variabilitySpecification>\n";
+		xml += getAPs(labelidmap);
+		xml += getSpecs(vs, labelidmap);
+		xml += "</variabilitySpecification>";
+		
+		return xml;
+	}
+
+	private static String getAPs(Map<String, String> labelidmap) {
+		String aps = "";
+		
+		aps += "\t<atomicPropositions>\n";
+		
+		for (String lbl: labelidmap.keySet()) {
+			aps += "\t\t<atomicProposition id=\"{" + labelidmap.get(lbl) + "}\" name=\"" + lbl + "\"/>\n";
+		}
+		
+		aps += "\t</atomicPropositions>\n";
+		
+		return aps;
+	}
+	
+	private static String getSpecs(VariabilitySpecification vs, Map<String, String> labelidmap) {
+		String specs = "";
+		
+		specs += "\t<specifications>\n";
+		
+		for (String sp: vs.getViresp()) {
+			specs += getSpecLine(sp, "always immediate response", labelidmap);
+		}
+		
+		for (String sp: vs.getViprec()) {
+			specs += getSpecLine(sp, "always immediate precedence", labelidmap);
+		}
+		
+		for (String sp: vs.getVeiresp()) {
+			specs += getSpecLine(sp, "exist immediate response", labelidmap);
+		}
+		
+		for (String sp: vs.getVerespReduced(false)) {
+			specs += getSpecLine(sp, "exist response", labelidmap);
+		}
+		
+		for (String sp: vs.getVconf()) {
+			specs += getSpecLine(sp, "always conflict", labelidmap);
+		}
+		
+		for (String sp: vs.getVpar()) {
+			specs += getSpecLine(sp, "exist parallel", labelidmap);
+		}
+		
+		specs += "\t</specifications>\n";
+		
+		return specs;
+	}
+	
+	private static String getSpecLine(String spec, String type, Map<String, String> labelidmap) {
+		String specline = "";
+		String source;
+		String reworkedspec = spec;
+		
+		for (String lbl: labelidmap.keySet()) {
+			reworkedspec = reworkedspec.replace("{" + lbl + "}", "{" + labelidmap.get(lbl) + "}");
+		}
+		
+		source = reworkedspec.substring(reworkedspec.indexOf("{") + 1, reworkedspec.indexOf("}"));
+		
+		specline += "\t\t<specification id=\"Import\" language=\"CTL\" type=\"" + type + "\" source=\"" + source + "\">" + reworkedspec + "</specification>\n";
+
+		return specline;
+	}
+}
